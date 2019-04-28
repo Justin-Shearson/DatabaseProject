9236d40161a413888dcf5f1f09e8c0f14474c994
justin-shearson <justin.shearson@case.edu>
2019-04-28 14:08:36 -0400

    Added dump file and begun creating addevent route

--- run.py
+++ run.py
@@ -24,6 +24,7 @@ def execute(sql):
 	cursor.close()
 	db.close()
 
+#Executes a login check to make sure that the username does not already exist
 @app.route("/execlogin", methods=['GET', 'POST'])
 def login():
 	username = str(request.form['username'])
@@ -39,6 +40,7 @@ def login():
 	else:
 		return "Username or password is incorrect!"
 
+#Routes to the page to check if the input username is already in use
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
 	username = str(request.form['username'])
@@ -49,6 +51,7 @@ def signup():
 	cursor.execute("SELECT name FROM Users u WHERE u.name = %s and u.password = %s", (username, password))
 	user = cursor.fetchall()
 
+	#If the user already exists
 	if len(user) != 0:
 		cursor.close()
 		database.close()
@@ -61,20 +64,29 @@ def signup():
 		database.close()
 		return redirect(url_for('index'))
 
+#Routes to the addevent page to add an event to the website
+@app.route("/addevent", methods=['GET', 'POST'])
+def addevent():
+	username = str(request.form['username'])
+	organizer = str(request.form['organizer'])
+	database = mysql.connector.connect(**config['mysql.connector'])
+	cursor = database.curosr()
+	cursor.execute("SELECT")
+
+#Used to render the webpage for the main website
 def index():
 	return render_template('index.html')
 
+#Routes to the main page of the website
 @app.route('/')
 def template_response():
 	return render_template('index.html')
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-# 	return render_template('index.html')
-
+#Currently used to route to the second page of the website
 @app.route('/secondpage')
 def secondpage():
 	return render_template('secondpage.html')
 
+#Run the server
 if __name__ == '__main__':
 	app.run(**config['app'])
